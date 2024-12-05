dc82c802750602fdbb65a2eceb390eb0518a1170
Mitesh Gupta <mgmiteshgupta134@gmail.com>
2024-12-05 22:04:12 +0530

    Add About Chatbot section with description and features in Streamlit

---
+++ pages/About Chatbot 💡.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+with st.expander("**About Chatbot**",icon="🤖"):
+        st.write(
+            "This chatbot is powered by advanced language models, designed to assist users by answering questions and providing helpful responses. "
+            "It is optimized for a wide range of queries and offers dynamic conversation management with message history, enabling more personalized interactions. 📚"
+        )
+        st.write(
+            "The chatbot uses cutting-edge AI models for efficient and accurate communication, with real-time updates, ensuring a seamless experience. "
+            "Whether you are looking for simple answers or more detailed insights, this chatbot is designed to meet your needs. 💡"
+        )
